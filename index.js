f895d8bb6da749c1f960e99ac13c16b7bad89d99
Mert Dönmez <81701134+benmertmiyim@users.noreply.github.com>
2022-12-19 16:22:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const functions = require("firebase-functions");
 
 var Iyzipay = require('iyzipay');
 
+const admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
+
+var db = admin.firestore();
+
+
+
 var iyzipay = new Iyzipay({
     apiKey: process.env.APIKEY,
     secretKey: process.env.SECRETKEY,
@@ -129,4 +138,49 @@ exports.delCard = functions.https.onRequest(async (req, res) => {
     else{
         return res.send("unauthorized attempt");
     }
+});
+
+exports.rating = functions.firestore
+    .document('vendors/{userId}/ratings/{ratingId}')
+    .onWrite( async (change, context) => {
+       
+        let totalSecurity = 0;
+        let totalAccessibility = 0;
+        let totalServiceQuality = 0;
+        let count = 0;
+
+        let path = 'vendors/' + context.params.userId + '/ratings';
+        let collecRef = db.collection(path);
+        let allComm = await collecRef.get().then(snapshot => {
+
+            snapshot.forEach(doc => {
+                let document = doc.data();
+                totalSecurity += document.security;
+                totalAccessibility += document.accessibility;
+                totalServiceQuality += document.serviceQuality;
+                count += 1;
+            });
+
+        }).catch(err => {
+            console.log('Error getting documents', err);
+        });
+
+        if(count != 0){
+            totalAccessibility = totalAccessibility / count;
+            totalSecurity = totalSecurity / count;
+            totalServiceQuality = totalServiceQuality / count;
+        }
+        
+        let rating = (totalAccessibility + totalSecurity + totalServiceQuality) / 3;
+        
+        let vendorRef = db.collection('vendors').doc(context.params.userId);
+        vendorRef.update({"security":totalSecurity,"accessibility":totalAccessibility,"serviceQuality":totalServiceQuality,"rating":rating});
+        return null;
+    });
+
+exports.addVendor = functions.firestore.document('vendors/{userId}')
+.onCreate( async (change, context) => {
+    let vendorRef = db.collection('vendors').doc(context.params.userId);
+    vendorRef.update({"active":false,"img_list":[],"hourly_price":10,"start_price":10,"accessibility":5,"rating":5,"security":5,"serviceQuality":5});
+    return null;
 });
